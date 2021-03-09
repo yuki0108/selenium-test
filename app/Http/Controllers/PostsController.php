136dc1184e8fc0ca1d02ae1fdefeba8f32a38302
先規　勇樹 <yukisaki80@gmail.com>
2021-03-09 09:42:51 +0900

    seleniumテスト

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,5 +10,8 @@ class PostsController extends Controller
 {
     public function index()
     {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+
+        return view('posts.index', ['posts' => $posts]);
     }
 }
